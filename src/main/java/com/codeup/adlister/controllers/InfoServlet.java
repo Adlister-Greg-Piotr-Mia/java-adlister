287192657583c9873ec8f65df069c069d13adef4
Mia <54636763+miamija7@users.noreply.github.com>
2022-11-15 11:29:26 -0600

    info.jsp, InfoServlet.java good connection

---
+++ src/main/java/com/codeup/adlister/controllers/InfoServlet.java
@@ -0,0 +1,23 @@
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.models.Ad;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "controllers.InfoServlet", urlPatterns = "/ads/info")
+public class InfoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int adId = Integer.parseInt(request.getParameter("id"));
+        request.setAttribute("adId", adId);
+
+        request.getRequestDispatcher("/WEB-INF/ads/info.jsp")
+                .forward(request, response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/ads/info.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Item Info</title>
+</head>
+<body>
+  <h1>Item Information</h1>
+  <p>Ad ID: ${adId}</p>
+  <p>Item: </p>
+  <p>Description: </p>
+  <p>Created By: </p>
+</body>
+</html>
