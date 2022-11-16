7126393ac8423e8b11f3d061373f4441f736fe12
Mia <54636763+miamija7@users.noreply.github.com>
2022-11-16 16:39:18 -0600

    show categories in info

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -4,6 +4,7 @@ public class DaoFactory {
     private static Ads adsDao;
     private static Users usersDao;
     private static Categories categoriesDao;
+    private static Ads_Categories ads_categoriesDao;
     private static Config config = new Config();
 
     public static Ads getAdsDao() {
@@ -25,5 +26,12 @@ public class DaoFactory {
             categoriesDao = new MySQLCategoriesDao(config);
         }
         return categoriesDao;
+    }
+
+    public static Ads_Categories getAds_CategoriesDao(){
+        if (categoriesDao == null){
+            ads_categoriesDao = new MySQLAds_CategoriesDao(config);
+        }
+        return ads_categoriesDao;
     }
 }
